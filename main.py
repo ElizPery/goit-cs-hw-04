992ff285215af52e4a8004900703be186a5d1871
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2025-03-15 09:35:56 +0000

    add searching for many words and exeptions handling

--- main.py
+++ main.py
@@ -8,46 +8,58 @@ lock = threading.Lock()
 shared_dict = {}
 
 # Function that searching word in text and add it to shared dictionary
-def find_word_thread(locker, file: str, word: str):
+def find_words_thread(locker, file: str, words):
     global shared_dict
-    formated_word: str = word.strip()
 
     # Opening file with context manager
-    with open(Path(file)) as f:
-        text = f.readlines()
+    try:
+        with open(Path(file)) as f:
+            text = f.readlines()
 
-        # Searching the word in lines one by one and if it is there add to the dictionary
-        for line in text:
-            if formated_word in line:
-                with locker:
-                    if formated_word in shared_dict.keys():
-                        shared_dict[f"{formated_word}"] = [*shared_dict.get(formated_word), file]
-                    else:
-                        shared_dict[f"{formated_word}"] = [file]
-                    return
+            # Searching the word in lines one by one and if it is there add to the dictionary
+            for word in words:
+                for line in text:   
+                    formated_word: str = word.strip()
+                    if formated_word in line:
+                        with locker:
+                            if formated_word in shared_dict.keys():
+                                shared_dict[f"{formated_word}"] = [*shared_dict.get(formated_word), file]
+                            else:
+                                shared_dict[f"{formated_word}"] = [file]
+                            break
+    except FileNotFoundError as not_found:
+        print(f"{not_found.filename} is not exist, please check name of the file!")
+    except:
+        print("Something went wrong, please try again!")
 
-def find_word_process(val, file: str, word: str):
-    formated_word: str = word.strip()
+def find_words_process(val, file: str, words: str):
 
     # Opening file with context manager
-    with open(Path(file)) as f:
-        text = f.readlines()
+    try:
+        with open(Path(file)) as f:
+            text = f.readlines()
 
-        # Searching the word in lines one by one and if it is there add to the manager dictionary
-        for line in text:
-            if formated_word in line:
-                if formated_word in val.keys():
-                    val[formated_word] = [*val[formated_word], file]
-                else:
-                    val[formated_word] = [file]
-                return
+            # Searching the word in lines one by one and if it is there add to the manager dictionary
+            for word in words:
+                for line in text:
+                    formated_word: str = word.strip()
+                    if formated_word in line:
+                        if formated_word in val.keys():
+                            val[formated_word] = [*val[formated_word], file]
+                        else:
+                            val[formated_word] = [file]
+                        break
+    except FileNotFoundError as not_found:
+        print(f"{not_found.filename} is not exist, please check name of the file!")
+    except:
+        print("Something went wrong, please try again!")
 
 def main():
     text_files = ["text_1.txt", "text_2.txt", "text_3.txt", "text_4.txt", "text_5.txt"]
     
     start_1 = time.perf_counter()
     # Create threads
-    threads = [threading.Thread(target=find_word_thread, args=(lock, file_name, "maximus")) for file_name in text_files]
+    threads = [threading.Thread(target=find_words_thread, args=(lock, file_name, ["maximus", "sit", "fnwekrf"])) for file_name in text_files]
 
     for t in threads: t.start()
     for t in threads: t.join()
@@ -61,10 +73,10 @@ def main():
     # Create processes
     with Manager() as manager:
         m = manager.dict()
-        processes = [Process(target=find_word_process, args=(m, file_name, "maximus")) for file_name in text_files]
+        processes = [Process(target=find_words_process, args=(m, file_name, ["maximus", "sit"])) for file_name in text_files]
        
         for p in processes: p.start()
-        for t in processes: t.join()
+        for p in processes: p.join()
 
         end_2 = time.perf_counter()
         print(f"Time of execution: {end_2 - start_2:.5f}")
